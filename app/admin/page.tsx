66afd77499824ab9978368d9db6e5e3e7431607d
rajabovz801-ai <rajabovz801@gmail.com>
2026-08-21 17:28:17 +0500

    Add admin route

---
+++ app/admin/page.tsx
@@ -0,0 +1,4 @@
+import { AdminClient } from '@/components/AdminClient';
+import { PlatformNav } from '@/components/PlatformNav';
+
+export default function AdminPage(){return <div className="platformRoot"><PlatformNav/><main className="platformMain"><AdminClient/></main></div>}
