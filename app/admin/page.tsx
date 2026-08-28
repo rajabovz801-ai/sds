4ca8da778e8d6dca373b45c08a664ee10f684544
rajabovz801-ai <rajabovz801@gmail.com>
2026-08-28 08:44:26 +0500

    Enable direct large HTML uploads in admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,7 @@
 import { AdminAttemptResetPanel } from '@/components/AdminAttemptResetPanel';
 import { AdminClient } from '@/components/AdminClient';
 import { AdminDailyTasksPanel } from '@/components/AdminDailyTasksPanel';
+import { AdminLargeHtmlUploadBridge } from '@/components/AdminLargeHtmlUploadBridge';
 import { AdminMenuPreview } from '@/components/AdminMenuPreview';
 import { AdminProfessionalLayer } from '@/components/AdminProfessionalLayer';
 import { AdminReadableTypography } from '@/components/AdminReadableTypography';
@@ -11,5 +12,5 @@ import { requireAdminServerSession } from '@/lib/auth/admin-server-session';
 
 export default async function AdminPage() {
   await requireAdminServerSession();
-  return <div className="adminRoot"><AdminReadableTypography /><AdminClient /><AdminDailyTasksPanel /><AdminMenuPreview /><AdminProfessionalLayer /><AdminResultsFilterFix /><AdminStudentResultsDropdown /><AdminTestManagerDropdown /><AdminAttemptResetPanel /></div>;
+  return <div className="adminRoot"><AdminLargeHtmlUploadBridge /><AdminReadableTypography /><AdminClient /><AdminDailyTasksPanel /><AdminMenuPreview /><AdminProfessionalLayer /><AdminResultsFilterFix /><AdminStudentResultsDropdown /><AdminTestManagerDropdown /><AdminAttemptResetPanel /></div>;
 }
