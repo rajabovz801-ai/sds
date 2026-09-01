585716a8049be698cb7be18fc72b9b33dbf396f6
rajabovz801-ai <rajabovz801@gmail.com>
2026-09-01 21:58:52 +0500

    style: enable professional overview polish

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,7 @@
 import { AdminClient } from '@/components/AdminClient';
 import { AdminLargeHtmlUploadBridge } from '@/components/AdminLargeHtmlUploadBridge';
 import { AdminMenuPreview } from '@/components/AdminMenuPreview';
+import { AdminOverviewProPolish } from '@/components/AdminOverviewProPolish';
 import { AdminPanelSafetyPolish } from '@/components/AdminPanelSafetyPolish';
 import { AdminProfessionalLayer } from '@/components/AdminProfessionalLayer';
 import { AdminReadableTypography } from '@/components/AdminReadableTypography';
@@ -13,5 +14,5 @@ import { requireAdminServerSession } from '@/lib/auth/admin-server-session';
 
 export default async function AdminPage() {
   await requireAdminServerSession();
-  return <div className="adminRoot"><AdminTestScopeBridge /><AdminLargeHtmlUploadBridge /><AdminReadableTypography /><AdminPanelSafetyPolish /><AdminClient /><AdminMenuPreview /><AdminProfessionalLayer /><AdminResultsFilterFix /><AdminStudentResultsDropdown /><AdminTestManagerDropdown /><AdminToolsHub /></div>;
+  return <div className="adminRoot"><AdminTestScopeBridge /><AdminLargeHtmlUploadBridge /><AdminReadableTypography /><AdminPanelSafetyPolish /><AdminClient /><AdminMenuPreview /><AdminProfessionalLayer /><AdminOverviewProPolish /><AdminResultsFilterFix /><AdminStudentResultsDropdown /><AdminTestManagerDropdown /><AdminToolsHub /></div>;
 }
