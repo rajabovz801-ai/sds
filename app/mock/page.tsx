e6503479ac465738e2a0a8ea609978967a9cfebf
rajabovz801-ai <rajabovz801@gmail.com>
2026-09-02 23:30:38 +0500

    feat: surface resumable practice on dashboard

--- app/mock/page.tsx
+++ app/mock/page.tsx
@@ -1,4 +1,5 @@
 import { ActiveMockBanner } from '@/components/ActiveMockBanner';
+import { DashboardContinuePracticePortal } from '@/components/DashboardContinuePracticePortal';
 import { DashboardSkillPerformancePortal } from '@/components/DashboardSkillPerformancePortal';
 import { DashboardSpeakingProgressPortal } from '@/components/DashboardSpeakingProgressPortal';
 import { StudentDashboardClient } from '@/components/StudentDashboardClient';
@@ -14,5 +15,5 @@ export default async function MockPage() {
     getDashboardData(student.id),
     getActiveMockForStudent(student.id),
   ]);
-  return <><StudentDashboardClient student={student} initialData={data} /><DashboardSpeakingProgressPortal /><DashboardSkillPerformancePortal initialData={data} /><ActiveMockBanner mock={activeMock} /></>;
+  return <><StudentDashboardClient student={student} initialData={data} /><DashboardContinuePracticePortal /><DashboardSpeakingProgressPortal /><DashboardSkillPerformancePortal initialData={data} /><ActiveMockBanner mock={activeMock} /></>;
 }
